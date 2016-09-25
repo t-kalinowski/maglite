34cc7ea2ab02c3df00da19dcc5b5692232178d3e
Tomasz Kalinowski <tkalinow@asu.edu>
2016-09-25 10:25:13 -0400

    initial commit

---
+++ R/maglite-package.r
@@ -0,0 +1,51 @@
+#' maglite.
+#'
+#' Makes available just the pipe operators from \pkg{magrittr} (hence the name,
+#' \strong{mag}rittr \strong{lite}). This is useful if you want to avoid any
+#' name conflicts that the magrittr aliases introduce.
+#'
+#' @details maglite makes the following functions available:
+#'   \itemize{
+#'      \item pipe \code{\link[magrittr]{\%>\%}}
+#'      \item compound assignment pipe \code{\link[magrittr]{\%<>\%}}
+#'      \item exposition pipe \code{\link[magrittr]{\%$\%}}
+#'      \item tee pipe \code{\link[magrittr]{\%T>\%}}
+#'   }
+#'
+#' @seealso
+#' \link[magrittr]{magrittr}
+#' \href{https://cran.r-project.org/web/packages/magrittr/vignettes/magrittr.html}{magrittr-vignette}
+#' @name maglite
+#' @docType package
+#'
+NULL
+
+
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname maglite
+#' @export
+NULL
+
+#' @importFrom magrittr %<>%
+#' @name %<>%
+#' @rdname maglite
+#' @export
+NULL
+
+#' @importFrom magrittr %$%
+#' @name %$%
+#' @rdname maglite
+#' @export
+NULL
+
+#' @importFrom magrittr %T>%
+#' @name %T>%
+#' @rdname maglite
+#' @export
+NULL
+
+
+
+
+
